8af05550bb27584b952b8817b2464f889b5e07b5
spacefinner <joshuaperrin@outlook.com>
2018-11-28 11:13:04 +1100

    new thing

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,12 +36,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bMillenials\b/g, "Kids These Days");
+	v = v.replace(/\bmillenials\b/g, "kids these days");
 	
 	textNode.nodeValue = v;
 }
-
 
